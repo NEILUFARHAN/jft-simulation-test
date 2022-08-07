ebc4ea69a24ed80361a1e4593b89de78747b549a
fachribandang <fachribandang2@gmail.com>
2022-08-07 10:29:56 +0700

    add index

---
+++ data/jft-main-db.js
@@ -0,0 +1,27 @@
+const sections = [
+    {
+        id:1,
+        name:"Script and Vocabulary",
+    },
+    {
+        id:2,
+        name:"Conversation and Expression",
+    },
+    {
+        id:3,
+        name:"Listening Comprehension",
+    },
+    {
+        id:4,
+        name:"Reading Comprehension",
+    }
+];
+
+const tests = [
+    {
+        id:1,
+        name:"Simulation JFT-1",
+        level:"A2",
+        questions:SimulationJFT1Questions
+    }
+];
