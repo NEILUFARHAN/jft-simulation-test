7a04c380a0dfeb39f062f2c526702f33e89d68a2
fachribandang <fachribandang2@gmail.com>
2022-08-16 18:52:16 +0900

    Update script.js
    bug in shuffle for evaluation result,
    unused the shuffle function until it fixed

--- script.js
+++ script.js
@@ -389,13 +389,13 @@
         return testQANavigationContainer;
     }
     const shuffle = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            const temp = array[i];
-            // Swap
-            array[i] = array[j];
-            array[j] = temp;
-        }
+        // for (let i = array.length - 1; i > 0; i--) {
+        //     const j = Math.floor(Math.random() * (i + 1));
+        //     const temp = array[i];
+        //     // Swap
+        //     array[i] = array[j];
+        //     array[j] = temp;
+        // }
         return array;
     };
     const createTestQA = (question) => {
